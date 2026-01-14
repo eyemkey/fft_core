5e3ae3508afd45a7556e88a7cc00fc919325dead
Arman Mkrtchyan <amkrtchyan1101@gmail.com>
2026-01-13 18:18:58 -0800

    butterfly_unit.sv implemented

---
+++ rtl/butterfly_unit.sv
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+
+
+module butterfly_unit #(
+    parameter WIDTH=16
+)
+(
+    input logic signed [WIDTH-1:0] in_a_re, 
+    input logic signed [WIDTH-1:0] in_a_im, 
+    input logic signed [WIDTH-1:0] in_b_re, 
+    input logic signed [WIDTH-1:0] in_b_im, 
+    
+    output logic signed [WIDTH-1:0] out_a_re, 
+    output logic signed [WIDTH-1:0] out_a_im, 
+    output logic signed [WIDTH-1:0] out_b_re, 
+    output logic signed [WIDTH-1:0] out_b_im 
+);
+
+    logic signed [WIDTH:0] sum_re_full, sum_im_full; //Extra bit to account for carry
+    logic signed [WIDTH:0] diff_re_full, diff_im_full;
+    
+    always_comb begin
+        sum_re_full = in_a_re + in_b_re; 
+        sum_im_full = in_a_im + in_b_im; 
+        
+        diff_re_full = in_a_re - in_b_re; 
+        diff_im_full = in_a_im - in_b_im; 
+        
+        out_a_re = sum_re_full >>> 1; // Dropping the LSB while keeping signs
+        out_a_im = sum_im_full >>> 1; // To keep memory usage consistent through stages
+        out_b_re = diff_re_full >>> 1; 
+        out_b_im = diff_im_full >>> 1;
+    end
+        
+endmodule
---
+++ rtl/fft_pkg.sv
@@ -0,0 +1,9 @@
+package fft_pkg;
+
+    typedef struct packed {
+        logic signed [15:0] re; 
+        logic signed [15:0] im;
+    } complex_t;
+
+
+endpackage
